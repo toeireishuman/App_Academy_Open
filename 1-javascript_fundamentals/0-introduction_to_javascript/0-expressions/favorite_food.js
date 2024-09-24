714ac498d5fd236cebbad43da7cc75392cf3329f
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-24 12:00:02 +0800

    Add solutions to practice exercises from expressions

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/0-expressions/favorite_food.js
@@ -0,0 +1,12 @@
+/*
+Let's write a program! Use console.log to print out your favorite food.
+Closely follow the syntax of the given console.log example.
+*/
+
+// Here is an example of using console.log:
+
+console.log("cereal");
+
+// Print out your favorite food using console.log below:
+
+console.log("Mango float");
---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/0-expressions/hello.js
@@ -0,0 +1,7 @@
+// Here is an example of using console.log to print a message:
+
+console.log("hello world");
+
+// Print out your own message using console.log below:
+
+console.log("Hello, App Academy Open!");
