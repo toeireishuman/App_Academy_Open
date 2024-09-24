6922e07b2bab50838add62e37ce28c8600b999ff
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-24 15:19:29 +0800

    Add my solution to the numbers_exercise

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/1-data_types/numbers_exercise.js
@@ -0,0 +1,15 @@
+/*
+Write 5 examples of expressions using each of the operations:
++, -, *, /, and %. Print out the result of your expressions
+using console.log. Be sure to test your work by running your code!
+*/
+
+// Example:
+console.log(4 + 2.5); // 6.5
+
+// Write 5 more expressions below:
+console.log(1 + 1 + 2);
+console.log(9 - 5);
+console.log(2 * (1 + 1));
+console.log(64 / 4 / 4);
+console.log(29 % 5 ** 2);
