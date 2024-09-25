2eb7b05e3d794b396d21f1a1053aeca42ed85760
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-25 09:32:19 +0800

    Add my solution to strings_exercise

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/1-data_types/strings_exercise.js
@@ -0,0 +1,15 @@
+/*
+Write 3 examples of expressions.
+One using + (concatenation), another using [] (indexing),
+and another using .length. Print out the result of your
+expressions using console.log. Be sure to test your
+work by running your code!
+*/
+
+// Example:
+console.log("hello " + "world"); // 'hello world'
+
+// Write 3 more expressions below:
+console.log("Hey" + ", " + "girlypop!");
+console.log("Girlypop!"[5]);
+console.log("Chikushou!".length);
