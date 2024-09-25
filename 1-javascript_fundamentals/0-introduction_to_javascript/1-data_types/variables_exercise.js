81cc2aae97ee8971ba8f664570d9e6735652610e
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-25 09:29:31 +0800

    Add my solution to variables_exercise

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/1-data_types/variables_exercise.js
@@ -0,0 +1,36 @@
+/*
+Time to practice variables! Follow the comments for instructions.
+Be sure to test your work by running your code!
+*/
+
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+
+let firstName;
+firstName = "Air";
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+let lastName;
+lastName = "Head";
+
+// 3. Declare a variable called `age`:
+
+let age;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+// 5. Assign the `age` variable to a number:
+
+age = 1000000;
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
