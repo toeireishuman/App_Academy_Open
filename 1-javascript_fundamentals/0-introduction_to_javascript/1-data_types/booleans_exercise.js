887b2918a20a47527b10456fa9257620b3fbb79c
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-25 08:56:33 +0800

    Add my solution to booleans_exercise

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/1-data_types/booleans_exercise.js
@@ -0,0 +1,13 @@
+/*
+Write 3 examples of expressions using each of the operations: !, &&, ||.
+Print out the result of your expressions using console.log.
+Be sure to test your work by running your code!
+*/
+
+// Example:
+console.log(false || false); // false
+
+// Write 3 more expressions below:
+console.log(!!!(false || !false));
+console.log(!(false || true));
+console.log(false || !!false || true);
