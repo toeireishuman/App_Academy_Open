49635bc607348204d82c65ccf6522d6c62f3db73
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-26 17:06:53 +0800

    Add my solution to goodbye.js

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/2-functions/goodbye.js
@@ -0,0 +1,13 @@
+/*
+Write a function goodbye(name) that takes in a
+string name and returns a string saying bye to
+that name. See the examples below.
+*/
+
+function goodbye(name) {
+	return `Bye ${name}.`;
+}
+
+console.log(goodbye("Daniel")); // => "Bye Daniel."
+console.log(goodbye("Mark")); // => "Bye Mark."
+console.log(goodbye("Beyonce")); // => "Bye Beyonce."
