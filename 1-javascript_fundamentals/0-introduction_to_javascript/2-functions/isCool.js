b2b8aee086a5d1cb1becaaacf397f3f343954c03
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-26 17:19:18 +0800

    Add my solution to isCool.js

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/2-functions/isCool.js
@@ -0,0 +1,21 @@
+/*
+We have provided a function named isCool. The isCool function
+will take a string argument and console.log and return a sentence
+saying that the given string "is cool!". Call the isCool function
+with the arguments listed below.
+*/
+
+function isCool(string) {
+	let coolSentence = string + " is cool!";
+	console.log(coolSentence);
+	return coolSentence;
+}
+
+// Call the above function passing in the below arguments:
+// Music
+// JavaScript
+// The world
+
+isCool("Music");
+isCool("Javascript");
+isCool("The world");
