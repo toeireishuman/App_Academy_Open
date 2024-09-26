848c480e778d6501a8cdb92f83d2dd38b71d36e3
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-26 17:34:13 +0800

    Add my solution to yell

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/2-functions/yell.js
@@ -0,0 +1,14 @@
+/*
+Write a function yell that takes in a string and returns
+a "yelled" version of that string.
+
+HINT: try using the .toUpperCase() function on a string
+to see what it does!
+*/
+
+function yell(str) {
+	return `${str.toUpperCase()}!!!`;
+}
+
+console.log(yell("I want to go to the store")); // => "I WANT TO GO TO THE STORE!!!"
+console.log(yell("Time to program")); // => "TIME TO PROGRAM!!!"
