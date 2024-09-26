0d4902584feceb40b4bba2c6746a2ee5ef9ea5bc
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-26 17:16:08 +0800

    Add my solution to muscle_memory

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/2-functions/muscle_memory.js
@@ -0,0 +1,22 @@
+/*
+Practice typing the syntax for functions.
+Write 10 functions with different messages
+and console.log them. Focus on using proper
+style and indentation. Be sure to test your
+code by running it after each repetition.
+You'll be surprised and also annoyed at what
+details you missed! Learn from those typos now
+and make your life easier.
+*/
+
+function sayHello(name) {
+	let msg = "Hello, " + name + ". How are you?";
+	return msg;
+}
+
+function saySayonara(name) {
+	return `Sayonara, ${name}.`;
+}
+
+console.log(sayHello("world"));
+console.log(saySayonara("girlypop"));
