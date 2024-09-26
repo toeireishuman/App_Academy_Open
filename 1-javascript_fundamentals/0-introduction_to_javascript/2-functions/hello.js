68455511bffccf75f4c459f3388d32ae39e513dc
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-26 17:22:27 +0800

    Add my solution to hello.js

---
+++ 1-javascript_fundamentals/0-introduction_to_javascript/2-functions/hello.js
@@ -0,0 +1,15 @@
+/*
+Write a function hello that takes in a string and prints out "Hello, "
+followed by the string.
+
+HINT: this function doesn't need to return anything, it should
+just print using console.log.
+*/
+
+function hello(str) {
+	console.log(`Hello, ${str}`);
+}
+
+hello("cat"); // prints "Hello, cat"
+hello("Helen"); // prints "Hello, Helen"
+hello("Sting"); // prints "Hello, Sting"
