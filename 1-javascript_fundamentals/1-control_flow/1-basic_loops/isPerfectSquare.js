656c67bafe69bd45d1437bd370d90f54c275b268
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-27 10:47:24 +0800

    Add my solution to isPerfectSquare

---
+++ 1-javascript_fundamentals/1-control_flow/1-basic_loops/isPerfectSquare.js
@@ -0,0 +1,28 @@
+/*
+Write a function isPerfectSquare that accepts a
+number as an argument. The method should return a
+boolean indicating whether or not the argument is a
+perfect square. A perfect square is a number that is
+the product of some number multiplied by itself.
+For example, since 64 = 8 * 8 and 144 = 12 * 12, 64 and
+144 are perfect squares; 35 is not a perfect square.
+*/
+
+function isPerfectSquare(num) {
+	for (let factor = 1; factor <= num; factor++) {
+		if (factor * factor === num) {
+			return true;
+		}
+	}
+	return false;
+}
+
+console.log(isPerfectSquare(1)); // true
+console.log(isPerfectSquare(4)); // true
+console.log(isPerfectSquare(64)); // true
+console.log(isPerfectSquare(100)); // true
+console.log(isPerfectSquare(169)); // true
+console.log(isPerfectSquare(2)); // false
+console.log(isPerfectSquare(40)); // false
+console.log(isPerfectSquare(32)); // false
+console.log(isPerfectSquare(50)); // false
