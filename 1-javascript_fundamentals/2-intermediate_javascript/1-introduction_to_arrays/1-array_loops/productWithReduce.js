0aef58e62d3e0342e4d2dfd2730b7670627d74a6
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-09-29 12:08:38 +0800

    Add my solution to productWithReduce

---
+++ 1-javascript_fundamentals/2-intermediate_javascript/1-introduction_to_arrays/1-array_loops/productWithReduce.js
@@ -0,0 +1,20 @@
+/*
+Write a function productWithReduce(nums) that takes in an array
+of numbers. The function should return the total product of multiplying
+all numbers of the array together. You can assume that nums will not be
+an empty array.
+*/
+
+// (array Number) -> Number
+// returns the total product of numbers inside the given array
+// Assume: given array is non-empty
+const productWithReduce = function (nums) {
+	let product = 1;
+	for (let i = 0; i < nums.length; i++) {
+		product *= nums[i];
+	}
+	return product;
+};
+
+console.log(productWithReduce([10, 3, 5, 2])); // 300
+console.log(productWithReduce([4, 3])); // 12
