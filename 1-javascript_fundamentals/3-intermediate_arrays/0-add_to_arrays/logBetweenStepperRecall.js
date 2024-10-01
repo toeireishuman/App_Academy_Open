7166683450a234a1a4ffdd5cf3595c46083b02a3
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-01 13:00:57 +0800

    Add my solution to logBetweenStepperRecall

---
+++ 1-javascript_fundamentals/3-intermediate_arrays/0-add_to_arrays/logBetweenStepperRecall.js
@@ -0,0 +1,31 @@
+/*
+Write a function logBetweenStepper(min, max, step)
+that takes in 3 numbers as parameters. The function
+should print out numbers between min and max at step
+intervals. See the following examples.
+
+Hint: this function only needs to print using console.log
+it does not need to return.
+*/
+
+const logBetweenStepper = function (min, max, step) {
+	for (let i = min; i <= max; i += step) {
+		console.log(i);
+	}
+};
+
+// Examples:
+logBetweenStepper(5, 9, 1); // prints out:
+// 5
+// 6
+// 7
+// 8
+// 9
+
+logBetweenStepper(-10, 15, 5); // prints out:
+// -10
+// -5
+// 0
+// 5
+// 10
+// 15
