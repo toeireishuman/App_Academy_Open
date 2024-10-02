7f85666fed87404e76f230488be3bba5bcb8a5f0
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-02 11:38:17 +0800

    Add my solution to rotateRight

---
+++ 1-javascript_fundamentals/3-intermediate_arrays/1-chop_up_arrays/rotateRight.js
@@ -0,0 +1,25 @@
+/*
+Write a function rotateRight(array, num) that takes in an array and a number
+as args. The function should return a new array where the elements of the array
+are rotated to the right num times. The function should not mutate the original
+array and instead return a new array.
+
+Define this function using function expression syntax.
+
+HINT: you can use Array's slice() method to create a copy of an array
+*/
+
+const rotateRight = function (array, num) {
+	const front = array.slice(array.length - num);
+	const back = array.slice(0, array.length - num);
+
+	return front.concat(back);
+};
+
+let arr = ["a", "b", "c", "d", "e"];
+console.log(rotateRight(arr, 2)); // [ 'd', 'e', 'a', 'b', 'c' ]
+console.log(arr); // [ 'a', 'b', 'c', 'd', 'e' ]
+
+let animals = ["wombat", "koala", "opossum", "kangaroo"];
+console.log(rotateRight(animals, 3)); // [ 'koala', 'opossum', 'kangaroo', 'wombat' ]
+console.log(animals); // [ 'wombat', 'koala', 'opossum', 'kangaroo' ]
