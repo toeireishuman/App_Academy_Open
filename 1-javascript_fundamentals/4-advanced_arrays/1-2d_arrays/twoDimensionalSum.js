5f9d9d7bfb7858759dbd25c027a5bdc3b5d611f9
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-03 13:23:51 +0800

    Add my solution to twoDimensionalSum

---
+++ 1-javascript_fundamentals/4-advanced_arrays/1-2d_arrays/twoDimensionalSum.js
@@ -0,0 +1,24 @@
+/*
+Write a function twoDimensionalSum(arr) that takes in a 2D array of numbers and
+returns the total sum of all numbers.
+*/
+
+function twoDimensionalSum(arr) {
+	let sum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		const subArray = arr[i];
+		for (let j = 0; j < subArray.length; j++) {
+			const number = subArray[j];
+			sum += number;
+		}
+	}
+
+	return sum;
+}
+
+let arr1 = [[1, 3], [-4, 7, 10], [2]];
+console.log(twoDimensionalSum(arr1)); // 19
+
+let arr2 = [[], [3, 1, 2]];
+console.log(twoDimensionalSum(arr2)); // 6
