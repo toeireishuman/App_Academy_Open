b9e12b6e74acd3f6d09f952e420c88e806b76774
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-05 15:13:33 +0800

    Add my solution to 15-choose-primes

--- 1-javascript_fundamentals/6-advanced_array_methods/1-advanced_array_methods_project/advanced_array_methods_project/problems/15-choose-primes.js
+++ 1-javascript_fundamentals/6-advanced_array_methods/1-advanced_array_methods_project/advanced_array_methods_project/problems/15-choose-primes.js
@@ -18,13 +18,23 @@ console.log(choosePrimes([5, 6, 4, 11, 2017])); // [ 5, 11, 2017 ]
 
 */
 
-let isPrime = function(num) {
-    // Your code here
-}
+let isPrime = function (num) {
+	if (num < 2) {
+		return false;
+	}
 
-let choosePrimes = function(nums) {
-    // Your code here
-}
+	for (let i = 2; i < num; i++) {
+		if (num % i === 0) {
+			return false;
+		}
+	}
+
+	return true;
+};
+
+let choosePrimes = function (nums) {
+	return nums.filter(isPrime);
+};
 
 // alternative solution using Array.filter
 // let choosePrimes = function(nums) {
@@ -34,7 +44,7 @@ let choosePrimes = function(nums) {
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
-    module.exports = choosePrimes;
+	module.exports = choosePrimes;
 } catch (e) {
-    module.exports = null;
-}
+	module.exports = null;
+}
